beab923e5c9061661960df9d4c10c3feccfe3778
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-07-31 16:17:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-//complete this code
-class Animal {}
+// 1. Animal class
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make sound
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// 2. Cat class (extends Animal)
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+// 3. Dog class (extends Animal)
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
